a16203b82078c0f01a8fdac2ce9dcebe0b85a6fd
Landon Taylor <landon.autumn.taylor@gmail.com>
2019-04-23 12:54:09 -0500

    mm

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -13,7 +13,7 @@ let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
 function printBoard() {
   for (let i = 0; i < board.length; i++) {
-    console.log(board[i]);
+    //console.log(board[i]);
   }
 }
 
@@ -28,13 +28,44 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
-  // your code here
+function generateHint(solution, guess) {
+  let solutionArray = solution.split('')
+  let guessArray = guess.split('')
+  let correctLetterLocations = 0;
+  let correctLetters = 0;
+  for (let i = 0; i < solutionArray.length; i++) {
+    if (solutionArray[i] === guessArray[i]) {
+      correctLetterLocations = correctLetterLocations + 1;
+      solutionArray[i] = null;
+    }
+  }
+  //console.log(correctLetterLocations);
+  //console.log(solutionArray);
+  
+  for (let j = 0; j < solutionArray.length; j++) {
+    let targetIndex = solutionArray.indexOf(guessArray[j]);
+    if (targetIndex > -1) {
+      correctLetters = correctLetters + 1;  
+    }
+  }
+  //console.log(correctLetters);
+  let hint = correctLetterLocations + '-' + correctLetters;
+  console.log(hint);
 }
 
 function mastermind(guess) {
-  solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
+  // solution = 'abcd'; // Comment this out to generate a random solution
+  if (guess === solution) {
+    console.log('You guessed it!')
+  } else {
+    generateHint(solution, guess)
+    console.log('Guess again')
+    board += 1;
+  }
+  if (board.length == 10) {
+    console.log('You ran out of turns! The solution was: ' + solution)
+    return 
+  }
 }
 
 
